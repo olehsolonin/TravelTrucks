f2bbca4686060a1f207765ad2c13bb1fdbe67240
Oleh <62122094+olehsolonin@users.noreply.github.com>
2024-11-16 00:07:53 +0200

    added new catalog items

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,12 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchCatalog } from '../../fetchReq.js';
+import css from '../Catalog/Catalog.module.css';
 
 export default function Catalog() {
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     async function getAllCatalog() {
       try {
-        const data = await fetchCatalog();
-        console.log(data.items);
+        const res = await fetchCatalog();
+        setData(res.items);
+        console.log(res);
+
         //   setTotalPages(data.total_pages);
       } catch (error) {
         console.log(error);
@@ -18,7 +23,32 @@ export default function Catalog() {
 
   return (
     <div>
-      <p>Catalog brat</p>
+      <h1>Latest articles</h1>
+
+      {data.length > 0 && (
+        <ul className={css.catalogContainer}>
+          {data.map(({ id, gallery, name }) => (
+            <li key={id} className={css.catalogItem}>
+              {/* <a
+                href={gallery[0].original}
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                {name}
+              </a> */}
+              <div className={css.imgContainer}>
+                <img
+                  src={gallery[0].thumb}
+                  alt="photo"
+                  className={css.catalogPhoto}
+                  width="292"
+                  height="320"
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }

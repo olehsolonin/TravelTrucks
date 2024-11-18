5530d981591d979fe8970d06d7c1fd1f0d36b0ba
Oleh <62122094+olehsolonin@users.noreply.github.com>
2024-11-18 23:59:33 +0200

    added Redux store

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+// 1. Імпортуємо провайдер
+import { Provider } from 'react-redux';
+// 2. Імпортуємо створений раніше стор
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true,
-      }}
-    >
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+// src/redux/store.js
+
+import { configureStore } from "@reduxjs/toolkit";
+
+// Поки що використовуємо редюсер який
+// тільки повертає отриманий стан Redux
+const rootReducer = (state, action) => {
+	return state;
+};
+
+export const store = configureStore({
+	reducer: rootReducer,
+});

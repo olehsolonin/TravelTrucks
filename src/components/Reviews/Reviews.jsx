dab464fbf112cdfa80f6f0524782061ed02cbc3d
Oleh <62122094+olehsolonin@users.noreply.github.com>
2024-12-09 23:20:15 +0200

    added Reviews component

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,3 +1,25 @@
+import { useSelector } from 'react-redux';
+import css from './Reviews.module.css';
+
 export default function Reviews() {
-  return <p>prava9 chastb</p>;
+  const allDetailsById = useSelector(state => state.details.items);
+  console.log(allDetailsById.reviews);
+  const reviews = allDetailsById.reviews;
+  return (
+    <div>
+      <ul className={css.reviewsContainer}>
+        {reviews.map((review, index) => {
+          return (
+            <li key={index}>
+              <div className={css.reviewNameStar}>
+                <p> {review.reviewer_name}</p>
+                <p>{review.reviewer_rating}</p>
+              </div>
+              <p> {review.comment}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
